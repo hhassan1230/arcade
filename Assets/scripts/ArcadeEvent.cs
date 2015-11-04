e3c890de8955dc470f51588ad46b079d744ae7a1
hhassan1230 <hhassan@oswego.edu>
2015-11-03 23:00:40 -0500

    commits with addition

---
+++ Assets/scripts/ArcadeEvent.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcadeEvent : MonoBehaviour {
+	public GameObject arcade;
+	private bool promptGame;
+	private string machine;
+
+	// Use this for initialization
+	void Start () {
+		promptGame = false;
+		machine = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (promptGame) {
+			settingNewLevel ();
+		}
+	}
+
+//	OnTriggerEnter( Collider thingIAmCollidingWith ){
+//			if(thingIAmCollidingWith.gameObject.tag == "Player"){
+//			print("I am colliding with the player");
+//		}
+	void OnTriggerEnter(Collider other) {
+			machine = arcade.gameObject.tag;
+			print("I am colliding with the " + arcade.gameObject.tag);
+		if(other.gameObject.tag == "Player"){
+			promptGame = true;
+
+//				print("I am colliding with the " + arcade.gameObject.tag);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		// Destroy everything that leaves the trigger
+		var machine = arcade.gameObject.tag;
+		if(other.gameObject.tag == "Player"){
+			promptGame = false;
+			print("I am no longer colliding with " + machine);
+		}
+	}
+
+	public void settingNewLevel(){
+			if (Input.GetKeyDown (KeyCode.P)) {
+				print ("I am pressing P");
+				switch (machine) {
+				case "Arcade_Runner":           
+					// Do Something
+					Application.LoadLevel ("RunningIntroScene");
+					
+					break;
+				case "Arcade_Driver":
+					// Do Something
+					Application.LoadLevel ("DrivingIntroScene");
+					break;
+				case "Arcade_Shooter":
+					// Do Something
+					Application.LoadLevel ("ShooterIntroScene");
+					break;
+				default:
+					// Do Something
+					print ("You got a Snack");
+					break;
+				}
+		}
+	}
+		
+}
---
+++ Assets/scripts/parallaxGameobject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class parallaxGameobject : MonoBehaviour {
+
+	public List<GameObject> bg = new List<GameObject>();
+	public float bgSize;
+	public float bgVelocity;
+	// Use this for initialization
+	
+	void Start () {
+
+		bg[1].transform.position = Vector2.zero;
+		bg[0].transform.position = Vector2.left * bgSize;
+		bg[2].transform.position = Vector2.right * bgSize;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveRight();
+	}
+	void moveRight(){
+		bg [0].transform.position -= (Vector3.right * Time.deltaTime * bgVelocity);
+		bg [1].transform.position -= (Vector3.right * Time.deltaTime * bgVelocity);
+		bg [2].transform.position -= (Vector3.right * Time.deltaTime * bgVelocity);
+		if (bg [1].transform.position.x <= -bgSize) {
+			bg.Insert(3, bg[0]);
+			bg.RemoveAt(0);
+			bg[2].transform.position = new Vector2(bg[1].transform.position.x + bgSize, 0);
+		}
+		
+	}
+}
